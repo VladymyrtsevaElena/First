ed3905fe0253d6cbb3a2ea54fdaaf5c1637a1e17
VladymyrtsevaElena <vladymyrtceva@gmail.com>
2021-11-28 01:11:49 +0200

    Найти число и его номер в списке

--- Найди меня.py
+++ Найди меня.py
@@ -1,3 +1,9 @@
 import random
 a = [random.randint(0,10) for i in range(10)]
 print(a)
+x = int(input("Введите число: "))
+if x in a:
+    a.index(x)
+    print("Число", x, "Находится в списке по номером", a.index(x))
+if x not in a:
+    print("Этого числа нет в списке")
